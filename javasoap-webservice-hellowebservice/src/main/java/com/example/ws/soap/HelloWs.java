b044b4a3e9c60f6538287ac12eb5111c16ffc36b
Moumita Ray <moumitacloud08@gmail.com>
2025-04-25 20:20:37 +0530

    commit all files

---
+++ javasoap-webservice-hellowebservice/src/main/java/com/example/ws/soap/HelloWs.java
@@ -0,0 +1,12 @@
+package com.example.ws.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class HelloWs {
+	@WebMethod
+	public String hello() {
+		return "Hello";
+	}
+}
